c8621f558c7f87f6fa3fb4a546bdf9c03e9484b7
Malaxor <nogobaduk@gmail.com>
2021-11-17 12:55:18 -0500

    completed initial setup; now client folder start script provides legacy support

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -2,14 +2,13 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Message = db.define("message", {
-  text: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  senderId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
+	text: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	senderId: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+	},
 });
-
 module.exports = Message;
